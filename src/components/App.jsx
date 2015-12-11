b81aade83c87d39b2815cf15638fa2f352884276
Yotam Barzilay <yotamb@wix.com>
2015-12-11 11:44:57 +0200

    results comp

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {List, Map} from 'immutable';
+
+const pair = List.of('Trainspotting', '28 Days Later');
+const tally = Map({
+    Trainspotting: 5,
+    '28 Days Later': 4
+});
+
+export default React.createClass({
+    render() {
+        return React.cloneElement(this.props.children, {
+            pair: pair,
+            tally: tally
+        });
+    }
+})
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Router, {Route} from 'react-router';
+import App from './components/App';
 import Voting from './components/Voting';
+import Results from './components/Results';
 
 require('./style.css');
 
-const pair = ['Trainspotting', '28 Days Later'];
+const routes = <Route component={App}>
+    <Route path="/results" component={Results}/>
+    <Route path="/" component={Voting}/>
+</Route>;
 
 ReactDOM.render(
-    <Voting pair={pair} />,
+    <Router>{routes}</Router>,
     document.getElementById('app')
 );
