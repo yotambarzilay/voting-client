97702a47b9b8d68af5cfc3b8a46a5b5d203ccefa
Yotam Barzilay <yotamb@wix.com>
2015-12-11 10:42:20 +0200

    styling

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Voting from './components/Voting';
 
+require('./style.css');
+
 const pair = ['Trainspotting', '28 Days Later'];
 
 ReactDOM.render(
-    <Voting pair={pair} winner="Trainspotting" />,
+    <Voting pair={pair} />,
     document.getElementById('app')
 );
